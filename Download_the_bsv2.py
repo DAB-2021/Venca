05f266e0820ee77a165e3804e0073e0a6c8204e8
Esnall <50551701+Esnall@users.noreply.github.com>
2022-03-23 17:11:22 +0100

    Add files via upload

--- Download_the_bsv2.py
+++ Download_the_bsv2.py
@@ -1,5 +1,4 @@
 #Selenium
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,18 +6,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+def loguj(text): #Metoda pro zapsani erroru do souboru.
+	print(text)
+	with open("logfile.txt", "a", encoding='utf-8') as logfile:
+		logfile.write(text+"\n")
+
 PATH = "C:\Program Files (x86)\chromedriver.exe" #Cesta k webdriveru
 driver = webdriver.Chrome(PATH) #jmeno
 
 compare = ["výroční zpráva [2020]","výroční zpráva [2019]","výroční zpráva [2018]","výroční zpráva [2017]"]
 ucetni = ["účetní závěrka [2020]","účetní závěrka [2019]","účetní závěrka [2018]","účetní závěrka [2017]"]
-# Je mozne ze download speed bude too fucking slow a nechci to checkovat pomoci download adresare kam se normalne veci stahuji
-# Takze kdytak upravit sleep timer dole na vice sekund (pokud by jste to opravdu chteli tak se obetuji)
+
 driver.get("https://aobp.cz/clenove/?fbclid=IwAR3ynXwAPAawMk5jmxG-uGajW9aTU_kBXEmjpiIMhgDPNhwAJzyO4xUTkcA")
+
 listveci = driver.find_elements_by_class_name("upme-name")
 listfirem = []
+
 for vec in listveci:
-	listfirem.append(vec.text)
+	listfirem.append(vec.text) #Vypis jednotlivych firem do array, ktere se budou hledat
 
 for link in listfirem: #Loop pro spolecnosti
 	driver.get("https://or.justice.cz/ias/ui/rejstrik")
@@ -31,14 +36,18 @@ for link in listfirem: #Loop pro spolecnosti
 		)
 		driver.find_element(By.PARTIAL_LINK_TEXT,"Sbírka").click()
 	except:
+		loguj(str(link) + " Neni na justice")
 		continue
+
 	i = -1
-	for zprava in compare: #Loop pro vyrocni zpravy.
+
+	for zprava in compare: #Loop typ zpravy
 		col = 0
 		i += 1
 		main = driver.find_element_by_class_name("list") #Lokace tablky
 		tbody = main.find_element(By.TAG_NAME, "tbody") #preskoceni nadpisu
 		rows = tbody.find_elements(By.TAG_NAME, "tr") #radky
+
 		for row in rows:
 			col = row.find_elements(By.TAG_NAME,"td")[1].text
 
@@ -59,6 +68,6 @@ for link in listfirem: #Loop pro spolecnosti
 		   	driver.execute_script("window.history.go(-1)")
 		   	time.sleep(1)
 		except:
-		  	print("test_error") #pokud to nenalezne odkaz (neni to na dalsi strance escape z loopu)
+		  	loguj(str(link) + " " + str(zprava)) #pokud to nenalezne odkaz (neni to na dalsi strance escape z loopu)
 
 driver.quit()
